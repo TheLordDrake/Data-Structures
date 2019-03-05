7284706d1d3cbada752ee7369e464d4dea46ad98
Iain F <iain.forsgren@silvertech.com>
2019-03-05 13:30:38 -0500

    Added Menu

--- Stack of Cards/Program.cs
+++ Stack of Cards/Program.cs
@@ -1,12 +1,46 @@
-﻿namespace Stack_of_Cards
+﻿using System;
+
+namespace Stack_of_Cards
 {
     internal class Program
     {
         public static void Main(string[] args)
         {
             var deck = new Deck();
+            var choice = 0;
             
-            deck.Draw(5);
+            do
+            {
+                Console.Write("What would you like to do?:\n"
+                                  + "1: Draw\n"
+                                  + "2: Shuffle\n"
+                                  + "0: Exit\n"
+                                  + "->: ");
+                
+                var input = Console.ReadLine();
+
+                if (string.IsNullOrWhiteSpace(input))
+                {
+                    continue;
+                }
+                
+                choice = int.Parse(input);
+
+                switch (choice)
+                {
+                    case 0:
+                        break;
+                    case 1:
+                        deck.Draw();
+                        break;
+                    case 2:
+                        deck.Shuffle();
+                        break;
+                    default:
+                        continue;
+                }
+
+            } while (choice != 0);
         }
     }
 }
